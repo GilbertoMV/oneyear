930544829f207dcded40597ebad1907ef948f66a
Gilberto Valenzuela <84487657+Gilberto0203@users.noreply.github.com>
2022-10-10 15:56:46 -0500

    avance seccion about you
    agrego las imagenes y los botones, sin función

--- assets/js/alertAbout.js
+++ assets/js/alertAbout.js
@@ -11,3 +11,80 @@ Swal.fire({
     },
     confirmButtonColor:'#475959',
 });
+
+$("#btn1").click(function(){
+    Swal.fire({
+        title: '¡mi museo!',
+        text: 'siempre te expreso la manera en la que mis ojos te ven, lo hermosa que eres, el brillo maravilloso que tienes y lo enamorado que estoy de cada parte de tí, nunca consigo que te veas con mis ojos, por ello aquí econtrarás una pestaña dedicada a lo maravillosa que eres.',
+        iconHtml: '💘',
+        iconColor:'#e7ded3',
+        background:"#e7ded3",
+        confirmButtonText: "pero yo más!",
+        customClass: {
+            container: 'text-alert',
+            input: 'input-alert',
+        },
+        confirmButtonColor:'#475959',
+    });
+});
+
+$("#btn2").click(function(){
+    Swal.fire({
+        title: '¡mi museo!',
+        text: 'siempre te expreso la manera en la que mis ojos te ven, lo hermosa que eres, el brillo maravilloso que tienes y lo enamorado que estoy de cada parte de tí, nunca consigo que te veas con mis ojos, por ello aquí econtrarás una pestaña dedicada a lo maravillosa que eres.',
+        iconHtml: '💘',
+        iconColor:'#e7ded3',
+        background:"#e7ded3",
+        confirmButtonText: "pero yo más!",
+        customClass: {
+            container: 'text-alert',
+            input: 'input-alert',
+        },
+        confirmButtonColor:'#475959',
+    });
+});
+$("#btn3").click(function(){
+    Swal.fire({
+        title: '¡mi museo!',
+        text: 'siempre te expreso la manera en la que mis ojos te ven, lo hermosa que eres, el brillo maravilloso que tienes y lo enamorado que estoy de cada parte de tí, nunca consigo que te veas con mis ojos, por ello aquí econtrarás una pestaña dedicada a lo maravillosa que eres.',
+        iconHtml: '💘',
+        iconColor:'#e7ded3',
+        background:"#e7ded3",
+        confirmButtonText: "pero yo más!",
+        customClass: {
+            container: 'text-alert',
+            input: 'input-alert',
+        },
+        confirmButtonColor:'#475959',
+    });
+});
+$("#btn4").click(function(){
+    Swal.fire({
+        title: '¡mi museo!',
+        text: 'siempre te expreso la manera en la que mis ojos te ven, lo hermosa que eres, el brillo maravilloso que tienes y lo enamorado que estoy de cada parte de tí, nunca consigo que te veas con mis ojos, por ello aquí econtrarás una pestaña dedicada a lo maravillosa que eres.',
+        iconHtml: '💘',
+        iconColor:'#e7ded3',
+        background:"#e7ded3",
+        confirmButtonText: "pero yo más!",
+        customClass: {
+            container: 'text-alert',
+            input: 'input-alert',
+        },
+        confirmButtonColor:'#475959',
+    });
+});
+$("#btn5").click(function(){
+    Swal.fire({
+        title: '¡mi museo!',
+        text: 'siempre te expreso la manera en la que mis ojos te ven, lo hermosa que eres, el brillo maravilloso que tienes y lo enamorado que estoy de cada parte de tí, nunca consigo que te veas con mis ojos, por ello aquí econtrarás una pestaña dedicada a lo maravillosa que eres.',
+        iconHtml: '💘',
+        iconColor:'#e7ded3',
+        background:"#e7ded3",
+        confirmButtonText: "pero yo más!",
+        customClass: {
+            container: 'text-alert',
+            input: 'input-alert',
+        },
+        confirmButtonColor:'#475959',
+    });
+});
